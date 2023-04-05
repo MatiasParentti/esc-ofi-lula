b36e500f24b05bdcae9463df5bde4415dd175254
MatiasParentti <matiparentti@gmail.com>
2023-04-05 12:52:15 -0300

    share fixed

--- src/components/compartir.jsx
+++ src/components/compartir.jsx
@@ -7,7 +7,7 @@ export default function Compartir() {
     const router = useRouter();
 
     const [url, setUrl] = useState(router.asPath);
-    const [what, setWhat] = useState(`https://api.whatsapp.com/send?text=${url}`);
+    const [what, setWhat] = useState(`https://api.whatsapp.com/send?text=https://esc-ofi-lula.vercel.app${url}`);
 
     useEffect(() => {
        
@@ -33,11 +33,10 @@ export default function Compartir() {
             <div className="section-actions is_area social-share">
 
                 <div className="hr"></div>
-                {url}
                 <p >Compartir en redes sociales</p>
                 <ul className="list-inline">
-                    <li><a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}><i className="fa-3x fab fa-facebook service-i"></i></a></li>
-                    <li><a href={`https://twitter.com/intent/tweet?text=Novedades%20de%20la%20escuela%20de%20oficios%20&url=${url}`}><i className="fab fa-twitter fa-3x  service-i"></i></a></li>
+                    <li><a href={`https://www.facebook.com/sharer/sharer.php?u=https://esc-ofi-lula.vercel.app${url}`}><i className="fa-3x fab fa-facebook service-i"></i></a></li>
+                    <li><a href={`https://twitter.com/intent/tweet?text=Novedades%20de%20la%20escuela%20de%20oficios%20&url=https://esc-ofi-lula.vercel.app${url}`}><i className="fab fa-twitter fa-3x  service-i"></i></a></li>
                     {/*       <li><a href="#"><i className="fab fa-3x fa-google-plus service-i"></i></a></li> */}
                     <li><a href={what}><i className="fa-3x fab fa-whatsapp service-i"></i></a></li>
 
