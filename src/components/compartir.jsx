ec17820c2c4ca157607a4fa13a002f98b1d77801
MatiasParentti <matiparentti@gmail.com>
2023-04-05 12:26:52 -0300

    test

--- src/components/compartir.jsx
+++ src/components/compartir.jsx
@@ -1,20 +1,27 @@
 import { useState } from "react"
 import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Compartir() {
 
+    const router = useRouter();
 
-    const [url, setUrl] = useState('');
-
-
+    const [url, setUrl] = useState(router.asPath);
+    const [what, setWhat] = useState(`https://api.whatsapp.com/send?text=${url}`);
 
     useEffect(() => {
+       
+        console.log(router.asPath)
+     
 
         return () => {
-            setUrl(window.location.href)
-            console.log(window.location.href)
+              setUrl(router.asPath)
+           
         }
 
     }, [])
+
+
 
 
     return (
@@ -26,12 +33,13 @@ export default function Compartir() {
             <div className="section-actions is_area social-share">
 
                 <div className="hr"></div>
+                {url}
                 <p >Compartir en redes sociales</p>
                 <ul className="list-inline">
                     <li><a href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}><i className="fa-3x fab fa-facebook service-i"></i></a></li>
                     <li><a href={`https://twitter.com/intent/tweet?text=Novedades%20de%20la%20escuela%20de%20oficios%20&url=${url}`}><i className="fab fa-twitter fa-3x  service-i"></i></a></li>
                     {/*       <li><a href="#"><i className="fab fa-3x fa-google-plus service-i"></i></a></li> */}
-                    <li><a href={`https://api.whatsapp.com/send?text=${url}`}><i className="fa-3x fab fa-whatsapp service-i"></i></a></li>
+                    <li><a href={what}><i className="fa-3x fab fa-whatsapp service-i"></i></a></li>
 
                 </ul>
             </div>
