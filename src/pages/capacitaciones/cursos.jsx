2478a491c9df073c3e9cb15ae403cc4d31188dbb
MatiasParentti <matiparentti@gmail.com>
2023-06-08 21:05:09 -0300

    fix

--- src/pages/capacitaciones/cursos.jsx
+++ src/pages/capacitaciones/cursos.jsx
@@ -27,25 +27,25 @@ export default function Cursos() {
                     </div>
 
 
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <h2 class="card-title">Carpintera/o en Aluminio Básico</h2>
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <h2 className="card-title">Carpintera/o en Aluminio Básico</h2>
                         </div>
-                        <img src="/img/carpintero.jpg" class="card-img-top" alt="..." />
-                        <div class="card-body text-center">
+                        <img src="/img/carpintero.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body text-center">
 
                             <Link target="_blank" className="btn py-2 px-lg-4  animated slideInLeft" href="https://forms.gle/DMA2D8QZ8FWmRCFz8">inscribite</Link>
                         </div>
                     </div>
 
-                    <div class="card mb-3 mt-5">
-                        <div class="card-body">
-                            <h2 class="card-title">Auxiliar en Instalaciones Sanitarias y de Gas -
+                    <div className="card mb-3 mt-5">
+                        <div className="card-body">
+                            <h2 className="card-title">Auxiliar en Instalaciones Sanitarias y de Gas -
                                 Armador de hierros para estructuras de Hormigón Armado
                             </h2>
                         </div>
-                        <img src="/img/marilo.jpg" class="card-img-top" alt="..." />
-                        <div class="card-body text-center">
+                        <img src="/img/marilo.jpg" className="card-img-top" alt="..." />
+                        <div className="card-body text-center">
                             <Link target="_blank" className="btn py-2 px-lg-4  animated slideInLeft" href="https://forms.gle/DMA2D8QZ8FWmRCFz8">inscribite</Link>
                         </div>
                     </div>
