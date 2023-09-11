9b900fc8bf6fadc39db0ce76377e73deacd6b069
MatiasParentti <matiparentti@gmail.com>
2023-09-11 14:02:58 -0300

    volantes

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-
+import Link from "next/link"
 export default function Banner() {
 
 
@@ -7,7 +7,21 @@ export default function Banner() {
 
         <div>
             <div className="container-xxl py-5">
+
+
                 <div className="container">
+
+                    <div className="text-center wow fadeInUp mb-5" data-wow-delay="0.1s">
+
+                        <div className="alert alert-info" role="alert">
+
+                            <h6> Acompaña con tu firma el reconocimiento de la Escuela de Oficios Inacio Lula Da Silva para su funcionamiento como Centro de Formación Profesional de la Provincia de Buenos Aires</h6>
+                            <div className="mt-2">
+                             
+                                <a className="btn" target="_blank" href="https://forms.gle/6aky3nSKs6GPNo8D7">Firma</a>
+                            </div>
+                        </div>
+                    </div>
                     <Image
                         className="img-fluid "
                         src="/img/banner.jpg"
