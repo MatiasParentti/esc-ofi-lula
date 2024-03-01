d106bfc4fe741fdc16c5363b99f86b27cb457edb
MatiasParentti <matiparentti@gmail.com>
2024-03-01 18:45:08 -0300

    noticia-firma

--- src/pages/noticias/apoyanos-con-tu-firma.jsx
+++ src/pages/noticias/apoyanos-con-tu-firma.jsx
@@ -1,6 +1,4 @@
 import Layout from "@/components/layout"
-//import Noticia from '../../models/Noticia';
-//import conectarDB from "@/lib/dbConnect";
 import LayotutNoticia from "@/components/noticia";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,7 +8,7 @@ export default function Test() {
 
     return (
 
-        <Layout share title={`Escuela de Oficios | Acto de inaguracion y comienzo de cursos`} description={`${'Este martes 4 de julio se realizo el Acto de inicio de cursada e inauguración de la Escuela Comunitaria de Oficios Inacio Lula Da Silva ubicada en el barrio de Marilo, Trujui, Moreno.'}`}>
+        <Layout share title={`Escuela de Oficios | Sumate a la campaña de reconocimiento de la Escuela`} description={`${'Sumate a la campaña de reconocimiento de la Escuela'}`}>
 
             <div className="container-fluid bg-primary py-5 mb-5 page-header">
                 <div className="container py-5">
@@ -28,19 +26,30 @@ export default function Test() {
 
             <div className="noticia container">
                 <div className="pt">
-                    <h1 className="mt-5">{'Apoyanos con tu firma'}</h1>
+                    <h1 className="mt-5">{'Sumate a la campaña de reconocimiento de la Escuela '}</h1>
                     <div className="hr"></div>
                     <div className="mt-5">
                         <h6>{'Acompaña con tu firma el reconocimiento de la Escuela de Oficios Inacio Lula Da Silva para su funcionamiento como Centro de Formación Profesional de la Provincia de Buenos Aires'}</h6>
                         <br></br>
                         <Image
                             className="img-fluid "
-                            src={'/img/ina.jpg'}
+                            src={'/img/d.JPEG'}
                             height={500}
                             width={500}
                             alt="banner"
                         ></Image>
 
+                        <br></br>
+                        <br></br>
+
+                        <p className="p-j">Nos encontramos en una campaña para lograr que la Escuela de Oficios Inacio Lula Da Silva sea reconocida como un Centro de Formación Profesional de la Provincia de Buenos Aires. Para ello iniciamos una red solidaria cuyo objetivo es sumar apoyo a este proyecto educativo, apoyando con tu firma y difundiendo en redes sociales.</p>
+                        <p className="p-j">La Escuela de Oficios Inacio Lula Da Silva nació como parte de un proyecto educativo orientado a la formación de jóvenes y adultos  e incluye a sectores sociales no alfabetizados y que no hayan completado el trayecto educativo de los niveles primario y secundario. Desde esta perspectiva la escuela integra la formación profesional, sociocultural y política de las y los estudiantes para una mejor inserción laboral en la sociedad.</p>
+                        <h5 className="p-j">¡Sumate, queremos llegar a las 500 mil firmas!</h5>
+                        
+                        <br></br>
+                        <div className="mt-2">
+                            <a className="btn" target="_blank" href="https://forms.gle/6aky3nSKs6GPNo8D7">Firma</a>
+                        </div>
                         <br></br>
                         <br></br>
 
--- src/pages/noticias/index.jsx
+++ src/pages/noticias/index.jsx
@@ -9,8 +9,8 @@ export default function Noticias() {
     return (
 
         <Layout title='Escuela de Oficios | Noticias' description='Novedades'>
-
             <Header title='Noticias'></Header>
+            <CardNoticia key={'apoyanos-con-tu-firma'} _id={'apoyanos-con-tu-firma'} title={'Sumate a la campaña de reconocimiento de la Escuela'} copete={'Nos encontramos en una campaña para lograr que la Escuela de Oficios Inacio Lula Da Silva sea reconocida como un Centro de Formación Profesional de la Provincia de Buenos Aires.'} body={'body'} important={'01 Marzo, 2024. 18:27 hrs'}></CardNoticia>
             <CardNoticia key={'acto-entrega-certificados'} _id={'acto-entrega-certificados'} title={'Acto de entrega de certificados en la Sede Marilo'} copete={'El sábado 7 de octubre se realizó en la Sede Marilo de la Escuela de Oficios Inacio Lula Da Silva el Acto de entrega de certificados.'} body={'body'} important={'10 Octubre, 2023. 16:22 hrs'}></CardNoticia>
             <CardNoticia key={'nuevos-cursos-en-septiembre-2023'} _id={'nuevos-cursos-en-septiembre-2023'} title={'Cursos de septiembre 2023'} copete={'Comenzamos la inscripción de los cursos que comenzarán en septiembre. Los mismos son  gratuitos,  mixtos  y están destinados a mayores de 18 años.'} body={'body'} important={'24 Julio, 2023. 18:22 hrs'}></CardNoticia>
             <CardNoticia key={'acto-de-inaguracion-comienzo-de-cursos'} _id={'acto-de-inaguracion-comienzo-de-cursos'} title={'Acto de inaguracion y comienzo de cursos'} copete={'Este martes 4 de julio se realizo el Acto de inicio de cursada e inauguración de la Escuela Comunitaria de Oficios Inacio Lula Da Silva ubicada en el barrio de Marilo, Trujui, Moreno.'} body={'body'} important={'6 Julio, 2023. 23:38'}></CardNoticia>
